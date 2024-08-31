b52929d6e943d7ad411baa8bf888f4b69d70c103
DOKOE3rd <groozals1234@gmail.com>
2024-08-31 23:22:54 +0700

    31082024 - 4

--- src/page/Worker_report.jsx
+++ src/page/Worker_report.jsx
@@ -1,29 +1,14 @@
 import React, { useState, useEffect } from "react";
 import rt_database from "../components/rt_database";
-import {
-  Button,
-  Modal,
-  Switch,
-  Flex,
-  Form,
-  Input,
-  Popconfirm,
-  message,
-  Table,
-  InputNumber,
-  Skeleton,
-} from "antd";
+import { Table, DatePicker, Skeleton, Divider } from "antd";
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+dayjs.extend(customParseFormat);
 
 const Worker_report = () => {
   const { getWorkerTimeRec, getWorkerRec, deleteWorkerTimeRec } = rt_database();
   const [load, setLoad] = useState(false);
-  const [startColumns, setStartcolumns] = useState([
-    {
-      title: "วันที่",
-      dataIndex: "date",
-      rowScope: "row",
-    },
-  ]);
+  const [startColumns, setStartcolumns] = useState([]);
   const [tbData, setTbdata] = useState([]);
 
   useEffect(() => {
@@ -35,8 +20,27 @@ const Worker_report = () => {
     const dynamicCo = take2.map((item) => ({
       title: item.fname,
       dataIndex: item.id,
+      render: (_, record) => {
+        const dtidx = item.id;
+        return (
+          <>
+            <p>เข้างาน: {record[dtidx]?.wEnter || "-"}</p>
+            <br />
+            <p>เบิก: {record[dtidx]?.borrow || "-"}</p>
+          </>
+        );
+      },
     }));
-    setStartcolumns([...startColumns, ...dynamicCo]);
+    const stCol = [
+      {
+        title: "วันที่",
+        dataIndex: "date",
+        rowScope: "row",
+        fixed: "left",
+      },
+    ];
+    //borrow
+    setStartcolumns([...stCol, ...dynamicCo]);
 
     const taketime = await getWorkerTimeRec();
     const uniqueTaketime = new Map();
@@ -53,17 +57,23 @@ const Worker_report = () => {
 
     const filteredTaketime = Array.from(uniqueTaketime.values());
     const unFilteredTaketime = Array.from(unUniqueTaketime.values());
-
+    // console.log(filteredTaketime);
     // Group items by date
     const genItems = filteredTaketime.reduce((acc, item) => {
       const existingItem = acc.find((i) => i.date === item.date);
 
       if (existingItem) {
-        existingItem[item.worker_id] = item.work_enter;
+        existingItem[item.worker_id] = {
+          wEnter: item.work_enter,
+          borrow: item.borrow, // Ensure consistent structure
+        };
       } else {
         acc.push({
           date: item.date,
-          [item.worker_id]: item.work_enter,
+          [item.worker_id]: {
+            wEnter: item.work_enter,
+            borrow: item.borrow,
+          },
         });
       }
 
@@ -78,8 +88,18 @@ const Worker_report = () => {
 
       return dateA - dateB;
     });
-    setTbdata(genItems);
-    // console.log(genItems); // This will display the generated items
+
+    const checkday = value || dayjs().format("MM-YYYY");
+    // console.log(checkday);
+    const filterGenitems = genItems.filter((item) => {
+      const dayBox = item.date.split("-");
+      const dayCheck = checkday.split("-");
+
+      return dayBox[1] === dayCheck[0] && dayBox[2] === dayCheck[1];
+    });
+    setTbdata([]);
+    setTbdata(filterGenitems);
+    // console.log(filterGenitems);
     // deleteWorkerTimeRec
     unFilteredTaketime.map((item) => {
       deleteWorkerTimeRec(item.id);
@@ -89,17 +109,34 @@ const Worker_report = () => {
     setLoad(true);
   };
 
+  const onDateChange = (date, dateString) => {
+    console.log(dateString);
+    callData(dateString);
+  };
+
   return (
     <>
       {load ? (
-        <Table
-          columns={startColumns}
-          dataSource={tbData}
-          bordered
-          scroll={{
-            x: 800,
-          }}
-        />
+        <>
+          <Divider />
+          <DatePicker
+            onChange={onDateChange}
+            format={"MM-YYYY"}
+            picker="month"
+            defaultValue={dayjs()}
+          />
+          <Divider />
+          <Table
+            columns={startColumns}
+            dataSource={tbData}
+            bordered
+            scroll={{
+              x: 1000,
+              y: 600,
+            }}
+            pagination={{ pageSize: 31 }}
+          />
+        </>
       ) : (
         <Skeleton active />
       )}
